305b54dc2cdeaf989e89b4ac30e308c593fb6625
Diep Nguyen <dng639045@gmail.com>
2021-05-06 22:06:19 +0200

    run-puppet in manifest geaendert.

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -2,7 +2,7 @@ file { '/usr/local/bin/run-puppet':
 	source	=> '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
 	mode	=> '0755',
 }
-cron { 'run puppet':
+cron { 'run-puppet':
 	command	=> '/usr/local/bin/run-puppet',
 	hour	=> '*',
 	minute	=> '*/15',
