f652d92b447c5e218ebaf5c8cd9890e48f7538b8
Diep Nguyen <dng639045@gmail.com>
2021-05-06 21:07:12 +0200

    run-puppet.pp geändert.

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -3,7 +3,7 @@ file { '/usr/local/bin/run-puppet':
 	mode	=> '0755',
 }
 cron { 'run puppet':
-	command	=> '/usr/local/bin/run-pupet',
+	command	=> '/usr/local/bin/run-puppet',
 	hour	=> '*',
 	minute	=> '*/15',
 }
